597df1cb45c6f526872a27a21ed4b2952bcf097d
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-01-05 20:14:56 +0800

    Refactor app initialization to use 'development' configuration and streamline run parameters

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
-import os
 from app import create_app
 from config import config
 
-# Get the configuration type from an environment variable, default to 'default'
-config_name = os.getenv('FLASK_CONFIG', 'default')
-
-# Create the app with the selected configuration
-app = create_app(config[config_name])
-
 if __name__ == '__main__':
+    config_name = 'development'
+    app = create_app(config_name)
     app.run(
-        host=config[config_name].HOST, 
-        port=config[config_name].PORT, 
-        debug=config[config_name].DEBUG
+        host=app.config['HOST'],
+        port=app.config['PORT'],
+        debug=app.config['DEBUG']
     )
