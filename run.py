8b6c809630edc864d97bb2a597b456ff50f860a0
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2025-01-14 23:01:13 +0800

    Add Vercel configuration file

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from app import create_app
 from config import config
 
+# Create the app instance for Vercel
+app = create_app('production')
+
 if __name__ == '__main__':
+    # This block will only run for local development
     config_name = 'development'
     app = create_app(config_name)
     app.run(
